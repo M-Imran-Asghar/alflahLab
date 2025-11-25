8683760bace45390244b3571f59a2a24562d9ce8
Muhammad Imran <145657017+M-Imran-Asghar@users.noreply.github.com>
2025-11-25 10:26:31 +0500

    Fix button variant for verification actions

--- src/app/dashboard/verification/page.tsx
+++ src/app/dashboard/verification/page.tsx
@@ -231,7 +231,7 @@ export default function VerificationPage() {
                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" /></svg>
                    Reject
                  </Button>
-                 <Button onClick={handleVerify} variant="success" className="flex-1 md:flex-none min-w-[140px] shadow-sm hover:shadow-md">
+                 <Button onClick={handleVerify}  className="flex-1 md:flex-none min-w-[140px] shadow-sm hover:shadow-md">
                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" /></svg>
                    Verify selected
                  </Button>
